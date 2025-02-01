337946e3e652fb86c94e9c572ec01e2c0a1ec2e7
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-02-01 16:01:28 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
 const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
-const STRIPE_SECRET_KEY = "sk_test_51OyB5mSAzKN1RgjbQyL7qPNuYbsFKyCSPxrzrbxwcyvA2fel7rw0M83CNC3ZvFe6EY5hT41ESfyTgx45GwWrfnwA00Fulon0rL"
+const STRIPE_SECRET_KEY = 
 
 export default{
     JWT_USER_PASSWORD,
